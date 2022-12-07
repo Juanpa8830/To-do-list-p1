fa055e94b655c417475cc66e5d2d5b7dba24b6de
Juanpa8830 <jp3_4@hotmail.com>
2022-12-07 17:20:24 -0500

    add static array and add script start at json file

--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
 import './styles/main.scss';
+import _, {constant} from "lodash"
 
-const userInput = document.querySelector('.task-input input');
-let tasks = JSON.parse(localStorage.getItem('Tasks'));
 const tasksList = document.getElementById('task-box');
 
-function showTasks() {
-  let li = '';
-  if (tasks) {
-    tasks.forEach((task, id) => {
-      li += `<li class="task">
-        <label for="${id}">
-            <input id="checkbox" value="true" type="checkbox">
-            <p>${task.description}</p>
-        </label>
-        <div class="settings">
-            <img class="dots" src="./dist/ellipsis-v.svg" alt="icon">
-            <ul class="task-menu">
-                <li><img class="dots" src="./dist/editpen.svg" alt="edit"></li>
-                <li><img class="dots" src="./dist/deletebin.svg" alt="edit"></li>
-            </ul>
-        </div>
-     </li>`;
-    });
-  }
-  tasksList.innerHTML = li;
-}
-showTasks();
+const tasks = [
+    {
+        id: 1,
+        description: 'complete microverse tasks',
+        "completed": "false"
+      },
+      {
+        id: 2,
+        description: 'feed my pet',
+        completed: "false"
+      },
+      {
+        id: 3,
+        description: 'make the bed',
+        completed: "false"
+      }
+];
 
-userInput.addEventListener('keyup', (e) => {
-  const userTask = userInput.value.trim();
-  if (e.key === 'Enter' && userTask) {
-    if (!tasks) {
-      tasks = [];
-    }
-    userInput.value = '';
-    const taskInfo = { description: userTask, status: 'pending' };
-    tasks.push(taskInfo);
-    localStorage.setItem('Tasks', JSON.stringify(tasks));
-    showTasks();
-  }
-});
+
+    for (let i=0; i<tasks.length; i+=1){
+        const data =`<li class="task">
+          <label for="${tasks[i].id}">
+              <input id="checkbox" value="true" type="checkbox">
+              <p>${tasks[i].description}</p>
+          </label>
+          <div class="settings">
+              <img class="dots" src="./dist/ellipsis-v.svg" alt="icon">
+              <ul class="task-menu">
+                  <li><img class="dots" src="" alt="edit"></li>
+                  <li><img class="dots" src="./dist/deletebin.svg" alt="edit"></li>
+              </ul>
+          </div>
+       </li>`;
+       tasksList.innerHTML += data;
+     };
+      
+    
+  
+  
+
+
+
+
+
